b84216f732b6ae4e7bc4cf87374556384ffac13e
aldridgeb <98923478+aldridgeb@users.noreply.github.com>
2022-05-04 13:51:20 +1200

    Assessment

---
+++ assmentment.py
@@ -0,0 +1,16 @@
+
+import random
+
+def get_name():
+    name_local = input("what is your name: ")
+    return name_local
+
+def get_age()
+    age_local = int(input("how old are you: "))
+    return age_local
+
+name = get_name()
+age = get_age()
+print(f"\nHi {name} at {age} years old you might find this a bit easy \n"
+      "\nanyway this is a test about days of the week \n"
+      ")

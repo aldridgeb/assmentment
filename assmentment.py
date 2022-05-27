3a76dd78a45a8db82b2670a98a1b4f759eb4c8d0
aldridgeb <98923478+aldridgeb@users.noreply.github.com>
2022-05-27 12:03:16 +1200

    Update assmentment.py

--- assmentment.py
+++ assmentment.py
@@ -1,22 +1,25 @@
 import random
 
-def get_name():
-    name_local = input("what is your name: ")
-    return name_local
-
-
 def get_age():
-    age_local = int(input("how old are you: "))
-    return age_local
+    age = ''
+    while not age.isnumeric():
+        age = input("What is your age:")
 
+    return age
 
-name = get_name()
-age = get_age()
-print(f"\nHi {name} at {age} years old you might find this a bit easy \n"
+def welcome():
+
+    name = input('What is your name')
+
+    age = get_age()
+
+    print(f"\nHi {name} at {age} years old you might find this a bit easy \n"
       "\nanyway this is a test about numbers in maroi \n"
-      "you will need to enter the number witch applies"
-      " e.g for the numbers write 1, 2 , 3 , 4 and so on \n")
+      "you will need to enter the number witch applies")
 
+
+
+welcome()
 days = [["tahi", "1"], ["rua", "2"], ["toru", "3"], ["wha", "4"], ["rima", "5"], ["ono", "6"], ["whitu", "7"], ["waru", "8"], ["tekau", "10"]]
 
 
